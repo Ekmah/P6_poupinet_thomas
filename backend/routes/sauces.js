2af0f5339acb4111dfc758535e0cf9501dcb079b
Thomas Poupinet <th.poupinet@free.fr>
2021-08-15 18:26:02 +0200

    reqLimiter, del pic when edit, indent, env exemple

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -5,12 +5,18 @@ const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-configs');
 
 const sauceCtrl = require('../controllers/sauces');
+const rateLimit = require("express-rate-limit");
+
+const reqLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100
+});
 
 router.get('/', auth, sauceCtrl.getAllSauces);
-router.post('/', auth, multer, sauceCtrl.createSauce);
+router.post('/', auth, reqLimiter, multer, sauceCtrl.createSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
-router.put('/:id', auth, multer, sauceCtrl.modifySauce);
-router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.put('/:id', auth, reqLimiter, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, reqLimiter, sauceCtrl.deleteSauce);
 router.post('/:id/like', auth, sauceCtrl.likeSauce)
 
 module.exports = router;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 const userCtrl = require('../controllers/user');
 const verifyPassword = require('../middleware/verifyPassword');
+const rateLimit = require("express-rate-limit");
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100
+});
 
-router.post('/signup', verifyPassword, userCtrl.signup);
+router.post('/signup', authLimiter, verifyPassword, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
